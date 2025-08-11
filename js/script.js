ebc315dce93b4fd6a905f6862971497361069a6d
Pedro Galvez <37886993+pgalvez9@users.noreply.github.com>
2025-08-11 17:41:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -55,9 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
             "title": "Inteligencia artificial en los vehículos",
             "time": "10:00 - 11:00",
             "speaker": {
-                "name": "Julieta Rojas",
-                "bio": "Ingeniera en Sistemas y Tecnologías de la Información por la Universidad Anáhuac México, actualmente me desempeño como Artificial Intelligence Engineer. Desde el área de Business Operations colaboro con los equipos de Product Development para identificar oportunidades de mejora, mapear procesos críticos, definir indicadores clave de desempeño (KPIs) que orienten la toma de decisiones y diseñar, desplegar y mantener soluciones de IA que automaticen tareas, optimicen la eficiencia operativa y mejoren la experiencia de usuario. Apasionada por la innovación y el aprendizaje continuo, combino pensamiento estratégico, creatividad y comunicación efectiva para convertir la ciencia de datos en valor tangible.",
-                "photo": "img/speaker-julieta-rojas.jpg",
 				"name": "Marco Ramírez",
                 "bio": "Ingeniero en Sistemas Computacionales en la Escuela Superior de Cómputo del IPN, me especializo en ciencia de datos en el área de Business Operations. Mis principales funciones incluyen apoyar a los empleados de PD en la adopción y uso de herramientas de inteligencia artificial dentro de Ford, colaborar en el diseño e implementación de estrategias clave para su integración, así como contribuir a la automatización de procesos y a la visualización de métricas alineadas con los objetivos y políticas tanto de los empleados como de la compañía.",
                 "photo": "img/speaker-marco-ramirez.jpg"
@@ -158,6 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
 
 
