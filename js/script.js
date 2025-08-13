533d5e3cad31bb20c334540fdff6036028b12817
EJ-by-Me <jimenez.lopez.e87@gmail.com>
2025-08-13 16:25:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
             "speaker": {
                 "name": "Marco Bello",
                 "bio": "Me llamo Marco Antonio Bello Xelhuantzi, soy Ingeniero en Diseño Automotriz por el Tec de Monterrey y cuento con la maestría en Innovación para el Desarrollo Empresarial por la misma institución, tengo 9 años de experiencia, desde el área de diseño en diversos commodities hasta mi puesto actual como Coordinador de Innovación para el área de Desarrollo de Producto de Ford de México, siendo mis principales responsabilidad la estrategia de innovación, la administración del laboratorio llamado Makerspace, además de los temas relacionados a propiedad intelectual.",
-                "photo": "img/speaker-marco-bello.jpg"
+                "photo": "img/speaker-marco-bello.JPG"
             },
             "activity": {
                 "description": " aún no la suben "
@@ -155,6 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
 
 
