325aebe1bdafe03a3bce6ce11d3a69a1be876079
EJ-by-Me <jimenez.lopez.e87@gmail.com>
2025-08-13 17:07:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
         "m-8": {
             "title": "Gemelos Digitales: La actualidad del desarrollo de controladores",
             "time": "14:00 - 15:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Ing Ignacio Salas",
                 "bio": "José Ignacio Salas Cerón is a mechatronic engineer with over 7 years of experience integrating digital twins of powertrain in the automotive industry.He has worked at Ford since 2018. His current role/title is HIL/SIL Integration and Deployment Engineer in the Vehicle Controls department.He enjoys taking part of challenging situations that require some sort of knowledge about the classical control theory to be resolved. He graduated from the Faculty of Engineering at UNAM, class of 2017.",
@@ -29,6 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		 "w-2": {
             "title": "Innovación en el Vehículo",
             "time": "10:00 - 11:00",
+			"audience": "General",
+			"location": "Auditorio",
             "activity": {
                 "description": " aún no la suben "
             },
@@ -38,9 +42,78 @@ document.addEventListener('DOMContentLoaded', () => {
                 "photo": "img/speaker-marco-bello.JPG"
             }
         },
+		"l-1": {
+            "title": "Inauguración / 100 años de Ingeniería en México",
+            "time": "09:00 - 10:00",
+			"audience": "General",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Juan Santillán",
+                "bio": "Egresado de la Universidad Anáhuac como Ingeniero Mecánico Eléctrico con una Maestría en Planeación Estratégica y Tecnologias, Juan Santillán cuenta con 25 años de experiencia en Ford Motor Company, incluyendo 9 años en asignaciones internacionales. Su trayectoria abarca desde la ingeniería de chasis para camiones y autos de segmento familiar en EE.UU. y el desarrollo de plataformas globales en Japón (Mazda), hasta el desarrollo, pruebas y validación de vehículos compactos en Alemania. De vuelta en México, lideró lanzamientos de nuevos vehículos y, como Global Chief Engineer, dirigió el desarrollo de productos globales como Ford Bronco Sport y Maverick. Reconocido por la revista Expansión como una de las 30 promesas en México,  actualmente, como Director de Desarrollo de Producto de Ford de México, lidera un equipo de más de 3400 ingenieros, impulsando el diseño, desarrollo y validación de vehículos a nivel mundial.",
+                "photo": "img/speaker-juan-santillan.png"
+            },
+            "activity": {
+                "description": "Ingeniero en Sistemas Computacionales en la Escuela Superior de Cómputo del IPN, me especializo en ciencia de datos en el área de Business Operations. Mis principales funciones incluyen apoyar a los empleados de PD en la adopción y uso de herramientas de inteligencia artificial dentro de Ford, colaborar en el diseño e implementación de estrategias clave para su integración, así como contribuir a la automatización de procesos y a la visualización de métricas alineadas con los objetivos y políticas tanto de los empleados como de la compañía.",
+				"photo": "img/speaker-marco-ramirez.jpg"
+            },
+		"l-2": {
+            "title": "Ingenieras Mexicanas",
+            "time": "10:00 - 11:00",
+			"audience": "General",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Stephanie Machorro, Michelle Jaramillo, Gabriela Vargas, Grisel Felipe",
+                "bio": "Stephanie Marín es ingeniera en gestión empresarial con maestría en ingeniería industrial. Con más de una década de experiencia en áreas como recursos humanos, ventas, manufactura, estimación de costos y gestión de programas, actualmente se desempeña como Program Manager en el área de Global Engineering Services dentro de Ford, liderando proyectos estratégicos de alto impacto. Apasionada por la ingeniería y el impulso del talento femenino en STEM, combina su carrera con su vida personal como esposa y madre de una niña pequeña, inspirando a otras mujeres a construir trayectorias profesionales plenas y equilibradas.",
+                "photo": "img/speaker-stephanie-marin.png"
+            },
+            "activity": {
+                "description": "TBD",
+				"photo": "img/speaker-marco-ramirez.jpg"
+            },
+		"l-3": {
+            "title": "Design Thinking - Taller",
+            "time": "11:00 - 12:30",
+			"audience": "Preselección",
+			"location": "CIA - Sótano",
+            "speaker": {
+                "name": "Jessica Mayo. Arturo Garcia, Uriel Loza",
+                "bio": "",
+                "photo": "img/speaker-jessica-mayo.png"
+            },
+            "activity": {
+                "description": "El equipo de Ford ha diseñado un taller para llevar a una selección de alumnos a través de el modelo de Design Thinking",
+				"photo": "img/speaker-marco-ramirez.jpg"
+            },
+		"l-4": {
+            "title": "Design Thinking",
+            "time": "11:00 - 12:00",
+			"audience": "General",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Jessica Mayo",
+                "bio": "TBD",
+                "photo": "img/speaker-jessica-mayo.png"
+            },
+            "activity": {
+                "description": "",
+				"photo": "img/speaker-marco-ramirez.jpg"
+            },
+		"l-5": {
+            "title": "Vehicle Showtime",
+            "time": "12:00 - 13:00",
+			"audience": "General",
+			"location": "Explanada CIA",
+            
+            },
+            "activity": {
+                "description": "Ven a conocer e interactuar con nuestros vehículos Ford",
+				"photo": "img/speaker-vehicle-showtime.jpg"
+            },
 		"l-6": {
             "title": "Elaboración de CV ",
             "time": "13:00 - 14:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Alexia Huerta",
                 "bio": "Profesional con formación en Diseño Industrial por la Universidad Anáhuac y una Maestría en Comunicación Digital, Redes Sociales y Marketing Online por el CEUPE. Cuenta con más de 6 años de experiencia en el desarrollo y gestión de estrategias de atracción de talento y programas enfocados en talento joven. Posee expertise en la implementación de estrategias externas orientadas a potenciar la marca empleadora. Es miembro de Ford desde agosto de 2023.",
@@ -50,21 +123,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 "description": "¿Quieres que tu CV destaque entre cientos de candidaturas? En esta ponencia, desvelaremos las claves para construir un CV ganador, centrándonos en el eficiente CV estilo Harvard y entenderás la Visión del Reclutador: qué buscan, cómo leen tu CV en segundos y cómo asegurarte de captar su atención."
             }
         },
-		"m-1": {
-            "title": "Inauguración / 100 años de Ingeniería en México",
-            "time": "09:00 - 10:00",
+		"l-7": {
+            "title": "Hexadrive",
+            "time": "14:00 - 15:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
-                "name": "Juan Santillán",
-                "bio": "Ingeniera en Sistemas y Tecnologías de la Información por la Universidad Anáhuac México, actualmente me desempeño como Artificial Intelligence Engineer. Desde el área de Business Operations colaboro con los equipos de Product Development para identificar oportunidades de mejora, mapear procesos críticos, definir indicadores clave de desempeño (KPIs) que orienten la toma de decisiones y diseñar, desplegar y mantener soluciones de IA que automaticen tareas, optimicen la eficiencia operativa y mejoren la experiencia de usuario. Apasionada por la innovación y el aprendizaje continuo, combino pensamiento estratégico, creatividad y comunicación efectiva para convertir la ciencia de datos en valor tangible.",
-                "photo": "img/speaker-juan-santillan.png"
+                "name": "Rafael Hernández",
+                "bio": "Rafael Hernández Cervantes es un ingeniero mecatrónico con una formación académica que incluye un MBA y estudios de maestría en Tecnologías de cómuputo y doctorado en proceso en Ciencias de la Computación. Con 12 años de trayectoria en Ford, lidera proyectos en los laboratorios de pruebas vehiculares, buscando generar ahorros a la compañía y mejorar la eficiencia operativa a través de la innovación proactiva.",
+                "photo": "img/speaker-rafael-hernandez.jpg"
             },
             "activity": {
-                "description": "Ingeniero en Sistemas Computacionales en la Escuela Superior de Cómputo del IPN, me especializo en ciencia de datos en el área de Business Operations. Mis principales funciones incluyen apoyar a los empleados de PD en la adopción y uso de herramientas de inteligencia artificial dentro de Ford, colaborar en el diseño e implementación de estrategias clave para su integración, así como contribuir a la automatización de procesos y a la visualización de métricas alineadas con los objetivos y políticas tanto de los empleados como de la compañía.",
-				"photo": "img/speaker-marco-ramirez.jpg"
-            },
+                "description": "¿Quieres que tu CV destaque entre cientos de candidaturas? En esta ponencia, desvelaremos las claves para construir un CV ganador, centrándonos en el eficiente CV estilo Harvard y entenderás la Visión del Reclutador: qué buscan, cómo leen tu CV en segundos y cómo asegurarte de captar su atención."
+            }
+        },
 		"m-3": {
             "title": "Inteligencia artificial en los vehículos",
             "time": "10:00 - 11:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Julieta Rojas & Marco Ramirez",
                 "bio": "Ingeniera en Sistemas y Tecnologías de la Información por la Universidad Anáhuac México, actualmente me desempeño como Artificial Intelligence Engineer. Desde el área de Business Operations colaboro con los equipos de Product Development para identificar oportunidades de mejora, mapear procesos críticos, definir indicadores clave de desempeño (KPIs) que orienten la toma de decisiones y diseñar, desplegar y mantener soluciones de IA que automaticen tareas, optimicen la eficiencia operativa y mejoren la experiencia de usuario. Apasionada por la innovación y el aprendizaje continuo, combino pensamiento estratégico, creatividad y comunicación efectiva para convertir la ciencia de datos en valor tangible.",
@@ -78,6 +155,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		"w-1": {
             "title": "¿Cómo se diseña un coche Ford?",
             "time": "09:00 - 10:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Estefhanie Gutiérrez",
                 "bio": "Ingeniera Mecánica con 9 años de experiencia práctica en el diseño de sistemas de exteriores e interiores y ornamentación automotriz. He colaborado en proyectos como el desarrollo de interiores para la Bronco Sport, Bronco y Mustang, combinando ingeniería con visión centrada en la experiencia del usuario. Con una maestría en Project Management por la Universidad de Barcelona y certificaciones como Scrum Master y Product Owner, reflejo de mi pasión por las metodologías ágiles, la mejora continua y la gestión de proyectos complejos y multidisciplinarios.",
@@ -90,6 +169,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		"w-5": {
             "title": "¿Cómo preparar tu entrevista?",
             "time": "13:00 - 14:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Karen Vázquez",
                 "bio": "Psicóloga organizacional con ocho años de experiencia en Recursos Humanos, especializada en atracción y desarrollo del talento. He tenido la oportunidad de liderar proyectos clave en empresas globales del sector de Telecomunicaciones y Automotriz, donde he contribuido al fortalecimiento de la cultura organizacional y al crecimiento del talento.",
@@ -102,6 +183,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		"j-3": {
             "title": "Simulación de Entrevista",
             "time": "14:00 - 15:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Karen Palacios",
                 "bio": "Más de 12 años de experiencia en Adquisición de Talento, especializada en perfiles de TI para empresas multinacionales en Norteamérica y Latinoamérica. Proceso de principio a fin, actuando como socio y consultor, alta demanda, liderando proyectos para BPO, RPO partnering y firmas de consultoría",
@@ -114,6 +197,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		"w-3": {
             "title": "Egresados en Ford",
             "time": "11:00 - 12:00",
+			"audience": "General",
+			"location": "Auditorio",
             "speaker": {
                 "name": "Oscar Carranza, Fabián Sánchez, Karla Ramírez, Rodrigo Rodríguez, Erick Ramos, Ma. Fernanda Landeros, Brenda Ávila",
                 "bio": "Falta",
@@ -180,6 +265,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
 
 
