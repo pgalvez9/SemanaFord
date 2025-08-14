281ffaaf47ebdb5dc8b825d3d944f9d933612374
EJ-by-Me <jimenez.lopez.e87@gmail.com>
2025-08-14 13:06:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 		},
 		"l-3": {
-            "title": "Design Thinking - Taller",
+            "title": "Taller de Design Thinking- Día 1",
             "time": "11:00 - 12:30",
 			"audience": "Preselección",
 			"location": "CIA - Sótano",
@@ -123,19 +123,19 @@ document.addEventListener('DOMContentLoaded', () => {
             "speaker": {
                 "name": "Jocelyn Moctezuma, Gabriela Tiscareño, Fernando Barajas y Rodrigo Téllez",
                 "bio": "TBD",
-                "photo": "img/speaker-julieta-rojas.png"
+                "photo": "img/speaker-jocelyn-moctezuma.png"
             },
             "activity": {
                 "description": "En esta sesión nos adentraremos en el mundo de los Ford Clubs, una iniciativa nacional diseñada para fomentar la integración, el bienestar y el espíritu de equipo entre empleados de diversas empresas a través de disciplinas deportivas, artísticas y más. Exploraremos qué son los Corporate Games y, específicamente, la experiencia de la participación de Ford en México, destacando cómo Ford ha impulsado activamente el desarrollo integral de sus colaboradores en este marco.",
             }
         },
 		"m-2": {
-            "title": "Inteligencia artificial en los vehículos",
-            "time": "10:00 - 11:00",
-			"audience": "General",
-			"location": "Auditorio",
+            "title": "SCRUM",
+            "time": "09:00 - 10:00",
+			"audience": "Preselección",
+			"location": "Sala",
             "speaker": {
-                "name": "Julieta Rojas & Marco Ramirez",
+                "name": "Pedro Gálvez",
                 "bio": "Ingeniera en Sistemas y Tecnologías de la Información por la Universidad Anáhuac México, actualmente me desempeño como Artificial Intelligence Engineer. Desde el área de Business Operations colaboro con los equipos de Product Development para identificar oportunidades de mejora, mapear procesos críticos, definir indicadores clave de desempeño (KPIs) que orienten la toma de decisiones y diseñar, desplegar y mantener soluciones de IA que automaticen tareas, optimicen la eficiencia operativa y mejoren la experiencia de usuario. Apasionada por la innovación y el aprendizaje continuo, combino pensamiento estratégico, creatividad y comunicación efectiva para convertir la ciencia de datos en valor tangible.",
                 "photo": "img/speaker-julieta-rojas.png"
             },
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         },
 		"m-3": {
-            "title": "Inteligencia artificial en los vehículos",
+            "title": "Inteligencia Artificial en los Vehículos",
             "time": "10:00 - 11:00",
 			"audience": "General",
 			"location": "Auditorio",
@@ -158,6 +158,58 @@ document.addEventListener('DOMContentLoaded', () => {
 				
             }
         },
+		"m-4": {
+            "title": "De la Facultad al Éxito",
+            "time": "11:00 - 12:00",
+			"audience": "General",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Francisco Lugo",
+                "bio": "Francisco Armando Lugo Mayorga es un destacado Ingeniero Mecatrónico, egresado de la Facultad de Ingeniería de la Universidad Nacional Autónoma de México (UNAM). Con una sólida trayectoria profesional, Francisco cuenta con más de 13 años de experiencia en la industria automotriz, específicamente en Ford Motor Company, donde ha desempeñado diversas funciones en áreas clave de ingeniería. Durante su carrera, ha demostrado una notable capacidad de liderazgo y gestión, lo que lo llevó a ocupar el cargo de Supervisor de Ingeniería en Liberaciones de Software a nivel mundial durante 3 años, cargo que sigue ocupando, aportando significativamente al desarrollo y la implementación de soluciones tecnológicas innovadoras. Además, Francisco ha complementado su formación técnica con un diplomado en Project Management, lo que le ha permitido dirigir proyectos complejos con eficiencia y eficacia, destacándose por su habilidad para coordinar equipos multidisciplinarios y asegurar el éxito en la entrega de resultados.",
+                "photo": "img/speaker-francisco-lugo.png"
+            },
+            "activity": {
+                "description": "¿Cómo transformar tu título de ingeniería en una carrera de impacto? En esta charla, compartiré mi viaje personal desde los salones de la facultad hasta la industria global. Les compartiré las habilidades clave y recomendaciones que para mí marcaron la diferencia.",
+            }
+        },
+		"m-5": {
+            "title": "Taller de Design Thinking- Día 2",
+            "time": "11:00 - 12:30",
+			"audience": "Preselección",
+			"location": "CIA - Sótano",
+            "speaker": {
+                "name": "Jessica Mayo. Arturo Garcia, Uriel Loza",
+                "bio": "",
+                "photo": "img/speaker-jessica-mayo.png"
+            },
+            "activity": {
+                "description": "El equipo de Ford ha diseñado un taller para llevar a una selección de alumnos a través de el modelo de Design Thinking",
+            }
+        },
+		"m-6": {
+            "title": "Vehicle Showtime",
+            "time": "12:00 - 13:00",
+			"audience": "General",
+			"location": "Explanada CIA",
+            "activity": {
+                "description": "Ven a conocer e interactuar con nuestros vehículos Ford",
+				"photo": "img/speaker-vehicle-showtime.jpg"
+            }
+        },
+		"m-7": {
+            "title": "Coloquio PARA - Experiencias compartidas por Ingenieros de Ford",
+            "time": "13:00 - 14:00",
+			"audience": "General y Agrupaciones",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Carlos Melgarejo, Ivonne Bárcenas y Pablo Gómez",
+                "bio": "TBD",
+                "photo": "img/speaker-carlos-melgarejo.png"
+            },
+            "activity": {
+                "description": "Una sesión de diálogo abierto que reúne a ingenieros experimentados de Ford y a estudiantes aspirantes de ingeniería, miembros del Programa de Alto Rendimiento Académico (PARA) de la Facultad de Ingeniería de la UNAM. Diseñada para cerrar la brecha entre el aprendizaje académico y los desafíos de ingeniería del mundo real, esta mesa redonda fomenta un intercambio de conocimientos. Los ponentes compartirán con los estudiantes cómo el PARA influyó en sus trayectorias profesionales y los estudiantes obtendrán conocimientos prácticos sobre el desarrollo de vehículos, metodologías de resolución de problemas y trayectorias profesionales directamente de líderes de la industria. Al mismo tiempo, los ingenieros de Ford podrán interactuar con la próxima generación de innovadores, comprendiendo filosofías de diseño emergentes y fomentando el talento futuro. Esta interacción dinámica tiene como objetivo inspirar, educar y cultivar una base sólida para la futura excelencia en ingeniería automotriz.",
+            }
+        },
         "m-8": {
             "title": "Gemelos Digitales: La actualidad del desarrollo de controladores",
             "time": "14:00 - 15:00",
@@ -169,9 +221,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			"photo": "img/speaker-ignacio-salas.jpg"
             },
             "activity": {
-                "description": "Nowadays, compressed vehicle development time, reduced costs, and optimized personnel utilization in the development and validation of new customer-expected features in vehicles,are imperative goals in the automotive industry.In that context, the development of digital twins to represent the operation of the powertrain and its adjacent subsystems, takes on special relevance. The digital twins provide costeffective and safe virtual environments that enable rapid development and testing of any type of control code.This presentation will cover the use of digital twins for powertrain control code development and testing at Ford.",
+                "description": "Hoy en día, la disminución de los tiempos de desarrollo de un automóvil, la reducción de costos y la optimización de la utilización de personal, así como la validación de nuevas funcionalidades altamente apreciadas por los consumidores, son metas esenciales en la industria automotriz. En ese contexto, el desarrollo de gemelos digitales para representar la operación de un tren motriz y sus subsistemas adyacentes, toma especial relevancia. Los gemelos digitales proveen entornos virtuales seguros y rentables, que permiten un desarrollo veloz y la puesta a prueba de cualquier tipo de código de control. Esta presentación cubrirá el uso de gemelos digitales del tren motriz para el desarrollo y puesta a prueba del código de controladores en Ford.",
             }
         },
+		/* HORARIO MIÉRCOLES */
 		"w-1": {
             "title": "¿Cómo se diseña un coche Ford?",
             "time": "09:00 - 10:00",
@@ -212,6 +265,18 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             "activity": {
                 "description": "Se trata de una dinámica en la que los estudiantes pueden hacer preguntas a los ingenieros que actualmente trabajan en Ford, que son egresados de la facultad de ingeniería sobre como es su día a día laboralmente, los retos que han enfrentado y las recomendaciones que les gustarían compartir con ellos para su desarrollo profesional y conocer la empresa. Registra tu asistencia en el siguiente link: https://forms.office.com/r/gz2WHDVxTe?origin=lprLink."
+			}
+			
+            },
+		"w-4": {
+            "title": "Vehicle Showtime",
+            "time": "12:00 - 13:00",
+			"audience": "General",
+			"location": "Explanada CIA",
+            "activity": {
+                "description": "Ven a conocer e interactuar con nuestros vehículos Ford",
+				"photo": "img/speaker-vehicle-showtime.jpg"
+            }
             },
 		"w-5": {
             "title": "¿Cómo preparar tu entrevista?",
@@ -227,6 +292,49 @@ document.addEventListener('DOMContentLoaded', () => {
                 "description": "¿Te sientes listo para afrontar una entrevista de trabajo con total confianza y maximizando tus posibilidades de éxito? En esta ponencia, te daremos las herramientas y conocimientos fundamentales para navegar el proceso de entrevista de manera efectiva. Descubrirás cómo presentarte de la mejor forma posible, comunicar tu valor de manera impactante y dejar una impresión memorable en los reclutadores."
             }
         },
+		"w-6": {
+            "title": "¿Cómo construir sistemas basados en agentes?",
+            "time": "14:00 - 15:00",
+			"audience": "General",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Leonardo Trujillo y Arturo Horcasitas",
+                "bio": "",
+                "photo": "img/speaker-arturo-horcasitas.jpg"
+            },
+            "activity": {
+                "description": "TBD"
+            }
+        },
+		"w-7": {
+            "title": "Experiencia de trabajo en equipo en Ford",
+            "time": "14:00 - 15:00",
+			"audience": "Preselección",
+			"location": "Sala",
+            "speaker": {
+                "name": "Roberto Peña y Enrique González",
+                "bio": "TBD",
+                "photo": "img/speaker-roberto-peña.jpg"
+            },
+            "activity": {
+                "description": "TBD"
+            }
+        },
+		"w-8": {
+            "title": "De 'Fórmula SAE' a la ingeniería automotriz, experiencias compartidas.",
+            "time": "15:00 - 16:30",
+			"audience": "General y Agrupaciones",
+			"location": "Auditorio",
+            "speaker": {
+                "name": "Santiago Aguilar, Paulo Becerril, Miguel Clemente, Salvador Honorato y Francisco Villa",
+                "bio": "TBD",
+                "photo": "img/speaker-santiago-aguilar.jpg"
+            },
+            "activity": {
+                "description": "TBD"
+            }
+        },
+			/* HORARIO JUEVES */
 		"j-1": {
             "title": "Shark tank presentation",
             "time": "10:00 - 11:00",
@@ -252,7 +360,6 @@ document.addEventListener('DOMContentLoaded', () => {
             "location": "Explanada CIA",
             "activity": {
                 "description": "Ven a conocer e interactuar con nuestros vehículos Ford."
-                "photo": "img/speaker-vehicle-showtime.jpg."
             }
         },
 		"j-4": {
@@ -295,7 +402,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 "description": "¡Reúnete con ex-bajas y descubre el camino del éxito en Ford de la mano de quienes lo iniciaron como tú! Ellos están listos para compartir su experiencia para ayudarte en el diseño de tu vehículo y brindar una perspectiva real sobre cómo sus estudios y experiencias como baja SAE se tradujeron en habilidades aplicables y éxito en su vida laboral. Aprovecha esta conexión única para inspirarte y aprender."
             }
         },
-		}
+		
+		
+		
 		
 		
         // Añade más actividades aquí, usando el ID correspondiente
@@ -354,6 +463,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
 
 
